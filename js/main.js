809f38901f71d0881167945c167493ed11a2f8e4
isimpledesign <isimpledesign@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-28 12:16:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/isimpledesign-approve-postspages-plugin/trunk@365772 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/main.js
@@ -0,0 +1,71 @@
+jQuery(document).ready(function() {
+	// main blog approve posts
+$('.check').click(function() {
+		 
+		var testor = $(this).parent().parent().html();
+		
+		$(this).parent().parent().empty();
+		 
+		$('#main_bloc_allready_approved').find('tbody').prepend('<tr>'+testor+'</tr>');
+		
+		var approve = $(this).attr("alt");
+		
+		$(this).hide();
+		
+		$('.load-' + approve).fadeIn('slow');
+				
+	var data = {
+		action: 'ajax_approve',
+		whatever: approve
+	};
+	// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+	jQuery.post(ajaxurl, data, function(response) {
+		
+	 }, "json" );				
+	 return false;
+});
+
+$('.check_already_approved').click(function() {
+											
+		var testor = $(this).parent().parent().html();
+		
+		$(this).parent().parent().empty();
+		
+		$('#main_bloc_approved').find('tbody').prepend('<tr>'+testor+'</tr>');
+		
+		var approve = $(this).attr("alt");
+		
+		$(this).hide();
+		
+		$('.load-' + approve).fadeIn('slow');
+		
+	var data = {
+		action: 'ajax_allapproved',
+		whatever: approve
+	};
+	// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+	jQuery.post(ajaxurl, data, function(response) {
+		
+	 }, "json" );				
+	 return false;
+});
+
+// approved - allready approved section
+
+$('.allready_approved').live('click', function(){
+				
+	 $('#main_bloc_approved').slideUp();
+	//location.reload();
+	 $('#main_bloc_allready_approved').fadeTo(0,1);				
+	 return false;
+});
+
+$('.approved').live('click', function(){
+			
+	$('#main_bloc_allready_approved').slideUp();
+	//location.reload();
+	$('#main_bloc_approved').fadeTo(0,1);	
+	 return false;
+});
+	
+});
