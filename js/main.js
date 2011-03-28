7e7b69abd7734e64e708f779074b5abc26f5cdf2
isimpledesign <isimpledesign@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-28 14:19:58 +0000

    git-svn-id: https://plugins.svn.wordpress.org/isimpledesign-approve-postspages-plugin/trunk@365816 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/main.js
+++ js/main.js
@@ -1,54 +1,61 @@
 jQuery(document).ready(function() {
 	// main blog approve posts
-$('.check').click(function() {
-		 
-		var testor = $(this).parent().parent().html();
-		
-		$(this).parent().parent().empty();
-		 
-		$('#main_bloc_allready_approved').find('tbody').prepend('<tr>'+testor+'</tr>');
-		
-		var approve = $(this).attr("alt");
-		
-		$(this).hide();
-		
-		$('.load-' + approve).fadeIn('slow');
-				
-	var data = {
-		action: 'ajax_approve',
-		whatever: approve
-	};
-	// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
-	jQuery.post(ajaxurl, data, function(response) {
-		
-	 }, "json" );				
-	 return false;
-});
+function bindEm() {
+	$('.check').live('click', function() {
+							   
+			$(this).removeClass('check').addClass('check_already_approved');
+			var testor = $(this).parent().parent().html();
+			$(this).parent().parent().empty();
+			$('#main_bloc_allready_approved').find('tbody').prepend('<tr>'+testor+'</tr>');
+			
+			var approve = $(this).attr("alt");
+			
+			$(this).hide();
+			
+			$(this).hide();
+			
+			$('.load-' + approve).fadeIn('slow');
+					
+		var data = {
+			action: 'ajax_approve',
+			whatever: approve
+		};
+		// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+		jQuery.post(ajaxurl, data, function(response) {
+			
+		 }, "json" );				
+		 return false;
+	});
+	
+	$('.check_already_approved').live('click', function(){
+			
+			$(this).removeClass('check_already_approved').addClass('check')
+			var testor = $(this).parent().parent().html();
+			$(this).parent().parent().empty();
+			
+			$('#main_bloc_approved').find('tbody').prepend('<tr>'+testor+'</tr>');
+			
+			//bindEm();	
+			
+			var approve = $(this).attr("alt");
+			
+			$(this).hide();
+			
+			$('.load-' + approve).fadeIn('slow');
+			
+		var data = {
+			action: 'ajax_allapproved',
+			whatever: approve
+		};
+		// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+		jQuery.post(ajaxurl, data, function(response) {
+			
+		 }, "json" );				
+		 return false;
+	});
+}
 
-$('.check_already_approved').click(function() {
-											
-		var testor = $(this).parent().parent().html();
-		
-		$(this).parent().parent().empty();
-		
-		$('#main_bloc_approved').find('tbody').prepend('<tr>'+testor+'</tr>');
-		
-		var approve = $(this).attr("alt");
-		
-		$(this).hide();
-		
-		$('.load-' + approve).fadeIn('slow');
-		
-	var data = {
-		action: 'ajax_allapproved',
-		whatever: approve
-	};
-	// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
-	jQuery.post(ajaxurl, data, function(response) {
-		
-	 }, "json" );				
-	 return false;
-});
+bindEm();	
 
 // approved - allready approved section
 
